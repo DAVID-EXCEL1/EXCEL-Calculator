e6734d2dbb3115ac2416a8a8e2bfa8e7b861edd2
David Excel <davidsome2004@gmail.com>
2024-01-20 02:52:16 +0100

    Ny Calculator For Excel

---
+++ script.js
@@ -0,0 +1,16 @@
+// Value Function ------------------------------
+
+function myValue(values) {
+    console.log(values);
+    display.innerHTML = values;
+}
+
+
+// AC Function ------------------------------
+function del() {
+    if (display.innerHTML == '') {
+        alert("Nothing to delete");
+    } else {
+        
+    }
+}
