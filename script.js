f1666085a1ea42dfa71639615893d9a972b5c3fe
David Excel <davidsome2004@gmail.com>
2024-01-25 11:36:36 +0100

    Dave

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 // Value Function ------------------------------
 
 function myValue(values) {
-    for (i=0; i <= values; i++){
-    console.log(values);
-    display.innerHTML = `${i}`;
-}
+    for (i = 0; i <= values; i++) {
+        console.log(values);
+        display.innerHTML = `${i}`;
+    }
 }
 
 // AC Function ------------------------------
 function del(del) {
-    var inputValue = document.getElementById("display").innerHTML;
+    let inputValue = document.getElementById("display").innerHTML;
     if (inputValue === "") {
         alert("Nothing to delete");
     } else {
         alert("About deleting your last digit")
         display.innerHTML = ""
+    }
+}
+
+
+// Equals To Function ---------------------------------------
+
+
+function solve() {
+    if (display.innerHTML === "") {
+        alert("Don't Cause Bug")
+    }else{
+        let answer = document.getElementById("ans");
+        confirm("Have you solved it")
+        answer = myValue(values)
     }
 }
